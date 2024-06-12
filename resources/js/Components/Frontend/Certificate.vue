b24a26e8944f9b3ce311c4890a1ed2ef94afde13
MarcoFonti <theemporiumg@gmail.com>
2024-06-13 00:00:54 +0200

    add edit to certificate

--- resources/js/Components/Frontend/Certificate.vue
+++ resources/js/Components/Frontend/Certificate.vue
@@ -6,30 +6,35 @@
     <!-- SEZIONE CERTIFICATO -->
     <div class="bg-light-tail-100 dark:bg-dark-secondary">
         <div class="container mx-auto py-12 px-4 sm:px-6 lg:flex lg:items-center lg:justify-center lg:py-16 lg:px-8">
-            
+
             <!-- CARD -->
-            <div class="flex bg-white border border-gray-200 rounded-lg shadow-md max-w-screen-xl mx-auto dark:border-gray-700 dark:bg-gray-800">
-                
+            <div
+            class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row dark:border-gray-700 dark:bg-gray-800">
+
                 <!-- IMMAGINE CERTIFICATO -->
-                <img class="object-cover w-48 h-96 md:h-auto md:w-48 lg:w-64 lg:h-auto rounded-l-lg md:rounded-l-none md:rounded-t-lg" src="../../../img/certificato-boolean.png" alt="">
-                
+                <img class="object-cover w-full h-auto rounded-t-lg md:w-48 md:rounded-none md:rounded-s-lg"
+                    src="../../../img/certificato-boolean.png" alt="Certificato Boolean">
+
                 <!-- CONTENUTO -->
-                <div class="flex flex-col justify-center gap-8 p-4 w-full">
-                    <h5 class="text-2xl font-bold tracking-tight text-gray-900 dark:text-white mb-2">Certificato di Completamento</h5>
-                    <p class="text-gray-700 dark:text-gray-400 mb-4">Ho completato il corso di 700 ore presso la Boolean Academy, focalizzato sullo sviluppo full-stack. Durante il corso, ho acquisito competenze avanzate in HTML, CSS, JavaScript, SQL e PHP, oltre ad aver lavorato con framework come Laravel e Vue.js. Sono entusiasta di applicare queste competenze per contribuire a progetti innovativi nel campo dello sviluppo software.</p>
+                <div class="flex flex-col justify-center gap-4 w-full">
+                    <h5 class="ml-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white mb-2 mt-3">Certificato di
+                        Completamento</h5>
+                    <p class="text-gray-700 dark:text-gray-400 ml-2 mr-2">Ho completato il corso di 700 ore presso la Boolean
+                        Academy, focalizzato sullo sviluppo full-stack. Durante il corso, ho acquisito competenze
+                        avanzate in HTML, CSS, JavaScript, SQL e PHP, oltre ad aver lavorato con framework come Laravel
+                        e Vue.js. Sono entusiasta di applicare queste competenze per contribuire a progetti innovativi
+                        nel campo dello sviluppo software.</p>
                     <div class="flex justify-end">
 
                         <!-- LINK BOOLEAN -->
-                        <a href="https://www.linkedin.com/school/boolean-careers/mycompany/" target="_blank" class="text-light-tail-500 hover:dark:text-dark-navy-100 hover:underline">Consulta i corsi Boolean</a>
+                        <a href="https://www.linkedin.com/school/boolean-careers/mycompany/" target="_blank"
+                            class="text-light-tail-500 hover:dark:text-dark-navy-100 hover:underline mr-6">Consulta i corsi
+                            Boolean</a>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+
+
 </template>
-
-
-
-
-
-
